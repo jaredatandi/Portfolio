907fbadce9544aee268a80402d6d994f976c5dd2
Anand8317 <43726580+Anand8317@users.noreply.github.com>
2022-11-16 14:19:33 +0530

    When an anchor tag is clicked, menu bar disappears as it should

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const toggleButton = document.getElementsByClassName('show-menu')[0];
 const closeButton = document.getElementsByClassName('x-icon')[0];
 const welcomeTitle = document.getElementsByClassName('welcome')[0];
 const navbarlinks = document.getElementsByClassName('navbar-links')[0];
+const a = document.getElementsByClassName('navbar-links');
 
 toggleButton.addEventListener('click', () => {
   navbarlinks.classList.toggle('active');
@@ -16,4 +17,13 @@ closeButton.addEventListener('click', () => {
   welcomeTitle.style.display = 'block';
   closeButton.style.display = 'none';
   toggleButton.style.display = 'block';
+});
+
+Array.from(a).forEach((element) => {
+  element.addEventListener('click', () => {
+    navbarlinks.classList.toggle('active');
+    welcomeTitle.style.display = 'block';
+    closeButton.style.display = 'none';
+    toggleButton.style.display = 'block';
+  })
 });
