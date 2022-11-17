c4df4f18904e81eed750660450c5c17c69ddc98a
jaredatandi <jaredatandi07@gmail.com>
2022-11-17 11:13:55 +0300

    Add the div to hold the popup page

--- index.js
+++ index.js
@@ -1,29 +1,84 @@
-// main JavaScript
+// Mobile section
 const toggleButton = document.getElementsByClassName('show-menu')[0];
 const closeButton = document.getElementsByClassName('x-icon')[0];
 const welcomeTitle = document.getElementsByClassName('welcome')[0];
 const navbarlinks = document.getElementsByClassName('navbar-links')[0];
 const navElements = document.getElementsByClassName('navbar-links');
 
-toggleButton.addEventListener('click', () => {
-  navbarlinks.classList.toggle('active');
-  welcomeTitle.style.display = 'none';
-  closeButton.style.display = 'block';
-  toggleButton.style.display = 'none';
-});
+let flag = false;
 
-closeButton.addEventListener('click', () => {
-  navbarlinks.classList.toggle('active');
-  welcomeTitle.style.display = 'block';
-  closeButton.style.display = 'none';
-  toggleButton.style.display = 'block';
-});
-
-Array.from(navElements).forEach((element) => {
-  element.addEventListener('click', () => {
+function toggleBtn() {
+  if (flag) {
     navbarlinks.classList.toggle('active');
     welcomeTitle.style.display = 'block';
     closeButton.style.display = 'none';
     toggleButton.style.display = 'block';
-  });
+    flag = false;
+  } else {
+    navbarlinks.classList.toggle('active');
+    welcomeTitle.style.display = 'none';
+    closeButton.style.display = 'block';
+    toggleButton.style.display = 'none';
+    flag = true;
+  }
+}
+
+toggleButton.addEventListener('click', toggleBtn);
+closeButton.addEventListener('click', toggleBtn);
+
+Array.from(navElements).forEach((element) => {
+  element.addEventListener('click', toggleBtn);
 });
+
+// Recent works
+const seeProject = document.getElementsByClassName('see-prj');
+const popup = document.getElementsByClassName('project-popup')[0];
+
+function displayPopup() {
+  popup.style.display = 'flex';
+  popup.innerHTML = `
+      <header>
+        <button class="close" style="white-space:pre-wrap; *white-space:initial;">&times;</button>
+        <div class="img-placeholder"></div>
+        <h1 class="font-fam">Keeping track of hundreds of components</h1>
+        <div class="prj-lang dflex font-fam">
+          <button class="lang">Ruby on rails</button>
+          <button class="lang">css</button>
+          <button class="lang">JavaScript</button>
+        </div>
+      </header>
+      <section class="dets">
+        <h2 class="font-fam">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it 1960s with the releaLorem Ipsum is simply dummy text of
+          the printing and typesetting industry. Lorem Ipsum has been the
+          industry's standard dummy text ever since the 1500s, when an unknown
+          printer took a galley of type and scrambled it 1960s with the relea
+        </h2>
+      </section>
+      <footer class="pop-buttons dflex">
+        <a href="#" class="green-back font-fam btn-container">
+          <button class="btn">See Live</button>
+          <img src="images/see live icon.svg" alt="see live" />
+        </a>
+        <a href="#" class="green-back font-fam">
+          <button class="btn">See source</button>
+          <img src="images/Vector.png" alt="see live" />
+        </a>
+      </footer>
+  `;
+}
+
+const close = document.getElementsByClassName('close')[0];
+
+Array.from(seeProject).forEach((element) => {
+  element.addEventListener('click', displayPopup);
+});
+
+function closePoup() {
+  popup.style.display = 'none';
+}
+
+close.addEventListener('click', closePoup);
