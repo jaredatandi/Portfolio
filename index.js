840311f68a015380e51ae7fa7b9c3c42388f1a94
jaredatandi <jaredatandi07@gmail.com>
2022-11-17 22:16:23 +0300

    Add form validation

--- index.js
+++ index.js
@@ -91,3 +91,16 @@ function closePoup() {
 }
 
 closeIcon.addEventListener('click', closePoup);
+
+const form = document.querySelector('.form-inputs');
+const email = document.getElementsByClassName('email')[0];
+const error = document.querySelector('.error');
+form.addEventListener('submit', (e) => {
+  const emailValue = email.value;
+  for (let i = 0; i < emailValue.length; i += 1) {
+    if (emailValue[i] !== emailValue[i].toLowerCase()) {
+      e.preventDefault();
+      error.innerText = 'Please enter email in lowercase!!!';
+    }
+  }
+});
